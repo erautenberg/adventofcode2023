36a686d8f1427015f7306352c6b9057048e53b35
Rautenberg <emily_rautenberg@comcast.com>
2023-11-30 18:19:09 -0500

    build: set up skeleton of AoC interactive answers

---
+++ README.md
@@ -0,0 +1,7 @@
+# adventofcode2023
+My solutions to Advent of Code 2023 (https://adventofcode.com/2023)
+
+## To Run:
+- `npm install`
+- `npm start`
+- Navigate to http://localhost:2023/
---
+++ public/src/1.js
@@ -0,0 +1,9 @@
+const DAY1 = 1;
+parseData(DAY1, (input) => {
+  const inputArray = input.map(n => parseInt(n));
+  const part1 = 'test';
+  const part2 = 'test';
+  // const part1 = runPart1(inputArray);
+  // const part2 = runPart2(inputArray);
+  showAnswers(DAY1, part1, part2);
+});
