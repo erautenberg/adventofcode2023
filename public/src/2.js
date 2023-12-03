0cf1fc4f8bfb171b1ee62e5de524277b4b2f1b68
Rautenberg <emily_rautenberg@comcast.com>
2023-12-03 00:20:28 -0500

    feat(Day2): Part 2

--- public/src/2.js
+++ public/src/2.js
@@ -1,16 +1,19 @@
 const DAY1 = 2;
 parseData(DAY1, (input) => {
-  console.log(formatGames(input));
   const parsedInput = formatGames(input);
+
   const validGames = Object.keys(parsedInput).map(id => isPossible(parsedInput[id], bagValues) && id).filter(Boolean);
   const part1 = getValidGameIdSum(validGames);
-  const part2 = '';
+
+  const fewestCubes = Object.keys(parsedInput).map(id => fewestPossible(parsedInput[id]))
+  const part2 = getSumOfPowers(fewestCubes);
+
   showAnswers(DAY1, part1, part2);
 });
 
 const colors = ['red', 'blue', 'green'];
 
-const formatGames = (games) => {
+const formatGames = games => {
   return games.reduce((acc, curr) => {
     const [ , id, counts] = curr.split(/Game (\d+): /gi);
     acc[id] = counts.split('; ').map(count => formatCounts(count));
@@ -18,7 +21,7 @@ const formatGames = (games) => {
   }, {});
 };
 
-const formatCounts = (count) => {
+const formatCounts = count => {
   const regex = new RegExp(`(?<num>\\d+) (?<color>${colors.join('|')})`, 'gi');
   return [...count.matchAll(regex)].map(match => match.groups);
 }
@@ -43,8 +46,31 @@ const isPossible = (game, bag) => {
   return possible;
 }
 
-const getValidGameIdSum = (ids) => {
+const getValidGameIdSum = ids => {
   return ids.reduce((acc, curr) => acc += parseInt(curr), 0)
 };
 
+const fewestPossible = game => {
+  let minRed = 0,
+      minGreen = 0,
+      minBlue = 0;
 
+  game.forEach(set => {
+    set.forEach(pull => {
+      let num = parseInt(pull.num);
+      if (pull.color === 'red' && minRed < num) {
+        minRed = num;
+      } else if (pull.color === 'green' && minGreen < num) {
+        minGreen = num;
+      } else if (pull.color === 'blue' && minBlue < num) {
+        minBlue = num;
+      }
+    });
+  });
+
+  return { minRed, minGreen, minBlue };
+}
+
+const getSumOfPowers = ids => {
+  return ids.reduce((acc, curr) => acc += (curr.minRed * curr.minGreen * curr.minBlue), 0);
+};
